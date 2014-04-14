34365d95f78dd087fc7f711bbfe34095208a7db9
Parker <parkerproject@gmail.com>
2014-04-14 17:34:10 -0400

    new files

---
+++ config/config.js
@@ -0,0 +1,23 @@
+exports.key = function () {
+    return '20fd8adb-a2ce-4f0a-bc32-b813d0a96eb5';
+};
+
+exports.mongodb = function () {
+    return 'mongodb://admin:admin123@ds033599.mongolab.com:33599/boxedsales';
+};
+
+exports.shopstyleKey = function () {
+    return 'uid3296-22786036-45';
+};
+
+exports.keywords = function () {
+    return encodeURIComponent('dresses clothing accessories');
+};
+
+exports.trackingID = function () {
+    return 8080337;
+}
+
+exports.pageNum = function () {
+    return 40;
+}
